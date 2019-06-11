18db1d964a9eeb64ca083da62c5e0ed4ac88328e
kavithahs77 <51526846+kavithahs77@users.noreply.github.com>
2019-06-11 11:02:22 +0530

    Add files via upload

---
+++ ComStack_cfg.h
@@ -0,0 +1,21 @@
+/*
+ * ComStack_cfg.h
+ *
+ *  Created on: 11-Jun-2019
+ *      Author: Admin
+ */
+
+
+#ifndef COMSTACK_CFG_H_
+
+#define COMSTACK_CFG_H_
+
+
+
+#include"ComStack_Types.h"
+
+
+
+
+
+#endif /* COMSTACK_CFG_H_ */
---
+++ Std_Types.h
@@ -0,0 +1,35 @@
+/*
+ * Std_Types.h
+ *
+ *  Created on: 11-Jun-2019
+ *      Author: Admin
+ */
+
+
+#ifndef STDTYPES_H_
+
+#define STDTYPES_H_
+
+
+
+#include"Platform_Types.h"
+
+typedef struct std{
+
+
+
+	uint16 vendorID;
+
+
+
+	uint16 moduleID;
+
+
+
+}
+
+Std_VersionInfoType ;
+
+
+
+#endif /* STDTYPES_H_ */
